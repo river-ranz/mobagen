94fd86091e55aafda50cd4682a5528254b2e8269
river <river.ranz@mymail.champlain.edu>
2024-08-29 12:32:12 -0400

    started work on cohesion- currently bugged

--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -8,6 +8,23 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // hint: iterate over the neighborhood
 
   // find center of mass
+  float centerMass = 0;
+  for (int i = 0; i < neighborhood.size(); i++) {
+    Vector2f pos = neighborhood[i]->getPosition();
+    centerMass += pos.x + pos.y;
+  }
+  centerMass /= neighborhood.size();
 
+  Vector2f boidPos = boid->getPosition();
+  float agent = boidPos.x + boidPos.y;
+  Vector2f agentCM = Vector2f(agent, centerMass);
+
+  if (agentCM.getMagnitude() > boid->getDetectionRadius()) {
+    cohesionForce = Vector2f(0,0);
+  }
+  else {
+    cohesionForce = agentCM / boid->getDetectionRadius();
+  }
+  
   return cohesionForce;
 }
