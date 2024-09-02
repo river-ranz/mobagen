a35702794ab6d938e6b7aac2a72fa9470d5d7948
River Ranz <river.ranz@mymail.champlain.edu>
2024-09-02 11:18:22 -0400

    fixed cohesion bug

--- examples/flocking/behaviours/AlignmentRule.cpp
+++ examples/flocking/behaviours/AlignmentRule.cpp
@@ -8,8 +8,8 @@ Vector2f AlignmentRule::computeForce(const std::vector<Boid*>& neighborhood, Boi
   // todo: add your code here to align each boid in a neighborhood
   // hint: iterate over the neighborhood
   if (!neighborhood.empty()) {
-    for (Boid *b : neighborhood) {
-      averageVelocity += b->velocity;
+    for (Boid *neighbor : neighborhood) {
+      averageVelocity += neighbor->velocity;
     }
     averageVelocity += boid->velocity;
     averageVelocity /= (neighborhood.size() + 1);
--- examples/flocking/behaviours/CohesionRule.cpp
+++ examples/flocking/behaviours/CohesionRule.cpp
@@ -8,26 +8,22 @@ Vector2f CohesionRule::computeForce(const std::vector<Boid*>& neighborhood, Boid
   // hint: iterate over the neighborhood
 
   // find center of mass
-  float centerMassX = 0;
-  float centerMassY = 0;
+  Vector2f centerMass = Vector2f::zero();
   if (!neighborhood.empty()) {
-    for (Boid *b : neighborhood) {
-      Vector2f pos = b->getPosition();
-      centerMassX += pos.x;
-      centerMassY += pos.y;
+    for (Boid *neighbor : neighborhood) {
+      centerMass += neighbor->getPosition();
     }
-  }
+    centerMass /= neighborhood.size();
 
-  Vector2f centerMass = Vector2f(centerMassX / neighborhood.size(), centerMassY / neighborhood.size());
+    Vector2f boidPos = boid->getPosition();
+    Vector2f agentCM = Vector2f(centerMass.x - boidPos.x, centerMass.y - boidPos.y);
 
-  Vector2f boidPos = boid->getPosition();
-  Vector2f agentCM = Vector2f(centerMass.x - boidPos.x, centerMass.y - boidPos.y);
-
-  if (agentCM.getMagnitude() > boid->getDetectionRadius()) {
-    cohesionForce = Vector2f(0,0);
-  }
-  else {
-    cohesionForce = Vector2f(agentCM.x / boid->getDetectionRadius(), agentCM.y / boid->getDetectionRadius());
+    if (agentCM.getMagnitude() > boid->getDetectionRadius()) {
+      cohesionForce = Vector2f(0,0);
+    }
+    else {
+      cohesionForce = Vector2f(agentCM.x / boid->getDetectionRadius(), agentCM.y / boid->getDetectionRadius());
+    }
   }
 
   return cohesionForce;
