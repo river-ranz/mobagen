6cb8febbb80a33728b24fcf827265ae8f9f31330
River Ranz <river.ranz@mymail.champlain.edu>
2024-09-12 11:15:48 -0400

    added to john conway file

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -5,14 +5,53 @@ void JohnConway::Step(World& world) {
   // todo: implement
 }
 
+bool topLeft(World& world, Point2D point) {
+  if (point.y%2==0) {
+    return world.Get({point.x - 1, point.y - 1});
+  }
+  else {
+    return world.Get({point.x, point.y - 1});
+  }
+}
+
+bool topRight(World& world, Point2D point) {
+  if (point.y%2==0) {
+    return world.Get({point.x + 1, point.y - 1});
+  }
+  else {
+    return world.Get({point.x, point.y - 1});
+  }
+}
+
+bool bottomLeft(World& world, Point2D point) {
+  if (point.y%2==0) {
+    return world.Get({point.x - 1, point.y + 1});
+  }
+  else {
+    return world.Get({point.x, point.y - 1});
+  }
+}
+
+bool bottomRight(World& world, Point2D point) {
+  if (point.y%2==0) {
+    return world.Get({point.x + 1, point.y + 1});
+  }
+  else {
+    return world.Get({point.x, point.y - 1});
+  }
+}
+
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   int acc = 0;
   for (int y = -1; y <= 1; y++) {
     for (int x = -1; x <= 1; x++) {
       Point2D p = point + Point2D(x, y);
-      // test if p is inside bounds
       acc += world.Get(p);
     }
   }
+  /*acc += topLeft(world, point) ? 1 : 0;
+  acc += topRight(world, point) ? 1 : 0;
+  acc += bottomLeft(world, point) ? 1 : 0;
+  acc += bottomRight(world, point) ? 1 : 0;*/
   return acc;
 }
