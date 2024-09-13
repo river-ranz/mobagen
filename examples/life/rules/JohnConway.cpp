184b18f802239a845eb1ac50207f5f76358db47d
river <132914629+panby34@users.noreply.github.com>
2024-09-13 01:13:41 -0400

    added conway step and fixed CountNeighbors

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -2,56 +2,70 @@
 
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
-  // todo: implement
+  for (int i = 0; i < world.SideSize(); i++) {
+    for (int j = 0; j < world.SideSize(); j++) {
+
+      bool isLive = world.Get({i, j});
+      int neighbors = CountNeighbors(world, Point2D(i, j));
+
+      //living cells with 2-3 neighbors live on
+      if (isLive && (neighbors == 2 || neighbors == 3))
+        world.SetNext(Point2D(i, j), true);
+
+      //dead cells with 3 neighbors get revived
+      else if (!isLive && neighbors == 3)
+        world.SetNext(Point2D(i, j), true);
+
+      //all other cells will die or stay dead
+      else
+        world.SetNext(Point2D(i, j), false);
+    }
+  }
 }
 
 bool topLeft(World& world, Point2D point) {
-  if (point.y%2==0) {
-    return world.Get({point.x - 1, point.y - 1});
-  }
-  else {
-    return world.Get({point.x, point.y - 1});
-  }
+  return world.Get({point.x - 1, point.y + 1});
+}
+
+bool topCenter(World& world, Point2D point) {
+  return world.Get({point.x, point.y + 1});
 }
 
 bool topRight(World& world, Point2D point) {
-  if (point.y%2==0) {
-    return world.Get({point.x + 1, point.y - 1});
-  }
-  else {
-    return world.Get({point.x, point.y - 1});
-  }
+  return world.Get({point.x + 1, point.y + 1});
+}
+
+bool middleLeft(World& world, Point2D point) {
+  return world.Get({point.x - 1, point.y});
+}
+
+bool middleRight(World& world, Point2D point) {
+  return world.Get({point.x + 1, point.y});
 }
 
 bool bottomLeft(World& world, Point2D point) {
-  if (point.y%2==0) {
-    return world.Get({point.x - 1, point.y + 1});
-  }
-  else {
-    return world.Get({point.x, point.y - 1});
-  }
+  return world.Get({point.x - 1, point.y - 1});
+}
+
+bool bottomCenter(World& world, Point2D point) {
+  return world.Get({point.x, point.y - 1});
 }
 
 bool bottomRight(World& world, Point2D point) {
-  if (point.y%2==0) {
-    return world.Get({point.x + 1, point.y + 1});
-  }
-  else {
-    return world.Get({point.x, point.y - 1});
-  }
+  return world.Get({point.x + 1, point.y - 1});
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   int acc = 0;
-  for (int y = -1; y <= 1; y++) {
-    for (int x = -1; x <= 1; x++) {
-      Point2D p = point + Point2D(x, y);
-      acc += world.Get(p);
-    }
-  }
-  /*acc += topLeft(world, point) ? 1 : 0;
+  //cycle through each neighbor and check if live
+  //if live, + 1 otherwise + 0
+  acc += topLeft(world, point) ? 1 : 0;
+  acc += topCenter(world, point) ? 1 : 0;
   acc += topRight(world, point) ? 1 : 0;
+  acc += middleLeft(world, point) ? 1 : 0;
+  acc += middleRight(world, point) ? 1 : 0;
   acc += bottomLeft(world, point) ? 1 : 0;
-  acc += bottomRight(world, point) ? 1 : 0;*/
+  acc += bottomCenter(world, point) ? 1 : 0;
+  acc += bottomRight(world, point) ? 1 : 0;
   return acc;
 }
