003016fe79e99f6906340681b2c073cd64247bba
River Ranz <river.ranz@mymail.champlain.edu>
2024-10-10 11:24:23 -0400

    work on borders

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -6,28 +6,34 @@ std::vector<Point2D> Agent::generatePath(World* w) {
 
   // bootstrap state
   auto catPos = w->getCat();
-  frontier.push(catPos);
-  frontierSet.insert(catPos);
+  AStarNode catStar;
+  catStar.point = catPos;
+  catStar.heuristic = distanceToTheBorder(catPos, w->getWorldSideSize()/2);
+  frontier.push(catStar);
+  frontierSet.insert(catStar);
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
 
   while (!frontier.empty()) {
     // get the current from frontier
-    Point2D p = frontier.front();
+    AStarNode p = frontier.top();
     // remove the current from frontierset
     frontier.pop();
     frontierSet.erase(p);
     // mark current as visited
-    visited.insert(make_pair(p, true));
+    visited.insert(make_pair(p.point, true));
 
     // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
-    vector<Point2D> visitableNeighbors = getVisitableNeighbors(w, p);
+    vector<Point2D> visitableNeighbors = getVisitableNeighbors(w, p.point);
     // iterate over the neighs:
     for (Point2D neighbor : visitableNeighbors) {
       // for every neighbor set the cameFrom
-      cameFrom.insert(make_pair(neighbor, p));
+      AStarNode neighborStar;
+      neighborStar.point = neighbor;
+      neighborStar.heuristic = distanceToTheBorder(neighbor, w->getWorldSideSize());
+      cameFrom.insert(make_pair(neighbor, p.point));
       // enqueue the neighbors to frontier and frontierset
-      frontier.push(neighbor);
-      frontierSet.insert(neighbor);
+      frontier.push(neighborStar);
+      frontierSet.insert(neighborStar);
       // do this up to find a visitable border and break the loop
     }
   }
--- examples/catchthecat/Agent.h
+++ examples/catchthecat/Agent.h
@@ -8,10 +8,39 @@
 
 class World;
 
+struct AStarNode {
+  Point2D point;
+  int accCost;
+  int heuristic;
+
+  bool operator<(const AStarNode& other) const {
+    return accCost + heuristic > other.accCost + other.heuristic;
+  }
+};
+
+int distanceToTheBorder(Point2D p, int SideSizeOver2) {
+  // right side
+  if (p.x - p.y < 0 && p.x + p.y > 0) {
+    return SideSizeOver2 - p.x;
+  }
+  // north
+  if (p.x - p.y > 0 && p.x + p.y > 0) {
+    return SideSizeOver2 - p.y;
+  }
+  // left side
+  if (p.x - p.y > 0 && p.x + p.y < 0) {
+    return SideSizeOver2 + p.x;
+  }
+  // south
+  if (p.x - p.y < 0 && p.x + p.y < 0) {
+    return SideSizeOver2 + p.y;
+  }
+}
+
 class Agent {
 private:
-  std::queue<Point2D> frontier;                   // to store next ones to visit
-  std::unordered_set<Point2D> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
+  std::priority_queue<AStarNode> frontier;          // to store next ones to visit
+  std::unordered_set<AStarNode> frontierSet;        // OPTIMIZATION to check faster if a point is in the queue
   std::unordered_map<Point2D, bool> visited;      // use .at() to get data, if the element dont exist [] will give you wrong results
 public:
   explicit Agent() = default;
